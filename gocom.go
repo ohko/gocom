7739436f51770a0892e01294f41c1fc63513ede0
ohko <ohko@qq.com>
2017-05-24 17:44:17 +0800

    增加三目运算和类型返回

--- gocom.go
+++ gocom.go
@@ -65,3 +65,47 @@ func Min(x, y interface{}) interface{} {
 		panic("type error")
 	}
 }
+
+// Ternary 三目运算
+func Ternary(b bool, x, y interface{}) interface{} {
+	if b {
+		return x
+	}
+	return y
+}
+
+// Type 获取对象的类型
+func Type(o interface{}) string {
+	switch o.(type) {
+	case int:
+		return "int"
+	case int8:
+		return "int8"
+	case int16:
+		return "int16"
+	case int32:
+		return "int32"
+	case int64:
+		return "int64"
+	case uint:
+		return "uint"
+	case uint8:
+		return "uint8"
+	case uint16:
+		return "uint16"
+	case uint32:
+		return "uint32"
+	case uint64:
+		return "uint64"
+	case float32:
+		return "float32"
+	case float64:
+		return "float64"
+	case string:
+		return "string"
+	case bool:
+		return "bool"
+	default:
+		return "interface{}"
+	}
+}
