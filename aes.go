52fc7c1af0e16554601f98e983befb4fc1cb235f
ohko <ohko@qq.com>
2018-04-12 16:50:15 +0800

    增加aes加密解密

---
+++ aes.go
@@ -0,0 +1,42 @@
+package gocom
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+func pkcs5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func pkcs5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+// AESCBCEncrypt aes加密数据
+// key的长度必须是：16、24、32
+func AESCBCEncrypt(key, data []byte) []byte {
+	out := make([]byte, len(data))
+	copy(out, data)
+	out = pkcs5Padding(out, aes.BlockSize)
+	c, _ := aes.NewCipher(key)
+	cipher.NewCBCEncrypter(c, key[:aes.BlockSize]).
+		CryptBlocks(out, out)
+	return out
+}
+
+// AESCBCDecrypt aes解密数据
+// key的长度必须是：16、24、32
+func AESCBCDecrypt(key, data []byte) []byte {
+	out := make([]byte, len(data))
+	copy(out, data)
+	c, _ := aes.NewCipher(key)
+	cipher.NewCBCDecrypter(c, key[:aes.BlockSize]).
+		CryptBlocks(out, out)
+	return pkcs5UnPadding(out)
+}
